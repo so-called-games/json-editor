c7ba543d33194bd9adab1087309e7f5d8033c49c
so-called-games <sorogincompany@gmail.com>
2024-11-14 05:04:41 +0500

    Added some filename options to the editor for things you can download, also now these options are saved in direct link

--- include/editor.js
+++ include/editor.js
@@ -1,54 +1,61 @@
 var defaultSchema = {}
 var data = {}
 var defaultOptions = Object.assign({}, JSONEditor.defaults.options, {
-	iconlib: 'fontawesome5',
-	object_layout: 'normal',
+	prompt_before_delete: false,
 	schema: defaultSchema,
-	show_errors: 'interaction',
-	theme: 'bootstrap4'
+	theme: "bootstrap3",
+	iconlib: "fontawesome5",
+	object_layout: "normal",
+	show_errors: "interaction"
 })
 var jsoneditor = null
-var directLink = document.querySelector('#direct-link')
-var booleanOptionsSelect = document.querySelector('#boolean-options-select')
-var head = document.getElementsByTagName('head')[0]
-var iconlibSelect = document.querySelector('#iconlib-select')
-var iconlibLink = document.querySelector('#iconlib-link')
-var libSelect = document.querySelector('#lib-select')
-var jsonEditorForm = document.querySelector('#json-editor-form')
-var objectLayoutSelect = document.querySelector('#object-layout-select')
-var setSchema = document.querySelector('#set-schema')
-var copySchema = document.querySelector('#copy-schema')
-var uploadSchema = document.querySelector('#upload-schema')
-var downloadSchema = document.querySelector('#download-schema')
-var schemaFilename = document.querySelector('#schema-filename')
-var setOutput = document.querySelector('#set-output')
-var copyOutput = document.querySelector('#copy-output')
-var uploadOutput = document.querySelector('#upload-output')
-var downloadOutput = document.querySelector('#download-output')
-var outputFilename = document.querySelector('#output-filename')
-var showErrorsSelect = document.querySelector('#show-errors-select')
-var themeSelect = document.querySelector('#theme-select')
-var themeLink = document.querySelector('#theme-link')
-var validateTextarea = document.querySelector('#validate-textarea')
+var directLink = document.querySelector("#direct-link")
+var resetButton = document.querySelector("#reset")
+var booleanOptionsSelect = document.querySelector("#boolean-options-select")
+var head = document.getElementsByTagName("head")[0]
+var iconlibSelect = document.querySelector("#iconlib-select")
+var iconlibLink = document.querySelector("#iconlib-link")
+var libSelect = document.querySelector("#lib-select")
+var jsonEditorForm = document.querySelector("#json-editor-form")
+var objectLayoutSelect = document.querySelector("#object-layout-select")
+var setOutput = document.querySelector("#set-output")
+var copyOutput = document.querySelector("#copy-output")
+var openOutput = document.querySelector("#open-output")
+var saveOutput = document.querySelector("#save-output")
+var outputFilename = document.querySelector("#output-filename")
+var setSchema = document.querySelector("#set-schema")
+var copySchema = document.querySelector("#copy-schema")
+var openSchema = document.querySelector("#open-schema")
+var saveSchema = document.querySelector("#save-schema")
+var schemaFilename = document.querySelector("#schema-filename")
+var showErrorsSelect = document.querySelector("#show-errors-select")
+var themeSelect = document.querySelector("#theme-select")
+var themeLink = document.querySelector("#theme-link")
+var validateTextarea = document.querySelector("#validate-textarea")
 var aceConfig = {
-	mode: 'ace/mode/json',
-	maxLines: Infinity,
-	minLines: 2,
+	mode: "ace/mode/json",
+	minLines: 4,
+	maxLines: 64,
 	showFoldWidgets: false,
 	showPrintMargin: false,
 	wrap: true
 }
-var outputTextarea = ace.edit('output-textarea', aceConfig)
-var schemaTextarea = ace.edit('schema-textarea', aceConfig)
-var ajvErrorsTextarea = ace.edit('ajv-errors-textarea', aceConfig)
-var jeErrorsTextarea = ace.edit('je-errors-textarea', aceConfig)
-var ajvErrorsCount = document.querySelector('#ajv-errors-count')
-var jeErrorsCount = document.querySelector('#je-errors-count')
+var outputTextarea = ace.edit("output-textarea", aceConfig)
+var schemaTextarea = ace.edit("schema-textarea", aceConfig)
+var ajvErrorsTextarea = ace.edit("ajv-errors-textarea", aceConfig)
+var jeErrorsTextarea = ace.edit("je-errors-textarea", aceConfig)
+var ajvErrorsCount = document.querySelector("#ajv-errors-count")
+var jeErrorsCount = document.querySelector("#je-errors-count")
 var ajv = new AjvValidator()
 String.prototype.replaceAll = function(search, replacement)
 {
 	var target = this;
-	return target.replace(new RegExp(search, 'g'), replacement);
+	return target.replace(new RegExp(search, "g"), replacement);
+}
+
+function replaceSpacings(dataToReplace)
+{
+	return dataToReplace.replaceAll("  ", "	")
 }
 
 function copyToClipboard(element)
@@ -60,18 +67,15 @@ function copyToClipboard(element)
 	tempArea.select()
 	document.execCommand("copy")
 	document.body.removeChild(tempArea)
+	document.documentElement.scrollTop = 0
+	document.body.scrollTop = 0
 }
 
-function replaceSpacings(dataToReplace)
+function openJSON(elementToSet, elementToClick)
 {
-	return dataToReplace.replaceAll("  ", "	")
-}
-
-function uploadFile(elementToSet, elementToClick)
-{
-	var tempInput = document.createElement('input')
-	tempInput.setAttribute('id', 'upload-input')
-	tempInput.type = 'file'
+	var tempInput = document.createElement("input")
+	tempInput.setAttribute("id", "upload-input")
+	tempInput.type = "file"
 	tempInput.click()
 	var content
 	
@@ -79,7 +83,7 @@ function uploadFile(elementToSet, elementToClick)
 	{
 		var file = e.target.files[0]
 		var reader = new FileReader()
-		reader.readAsText(file, 'UTF-8')
+		reader.readAsText(file, "UTF-8")
 		reader.onload = readerEvent =>
 		{
 			elementToSet.setValue(readerEvent.target.result)
@@ -90,7 +94,7 @@ function uploadFile(elementToSet, elementToClick)
 	}
 }
 
-function downloadFile(data, filename, type)
+function saveJSON(data, filename, type)
 {
 	var file = new Blob([data], {type: type})
 	
@@ -116,13 +120,13 @@ const validateSchema = () =>
 {
 	const dataToValidate = JSON.parse(schemaTextarea.getValue())
 	let errors
-	ajvErrorsCount.textContent = '0'
+	ajvErrorsCount.textContent = "0"
 	errors = ajv.validate202012(dataToValidate)
 	ajvErrorsTextarea.setValue(replaceSpacings(JSON.stringify(errors, null, 2)))
 	ajvErrorsCount.textContent = JSON.stringify(errors.length)
 	schemaTextarea.clearSelection(1)
 	ajvErrorsTextarea.clearSelection(1)
-	jeErrorsCount.textContent = '0'
+	jeErrorsCount.textContent = "0"
 	errors = ajv.validateJeMetaSchema(dataToValidate)
 	jeErrorsTextarea.setValue(replaceSpacings(JSON.stringify(errors, null, 2)))
 	jeErrorsCount.textContent = JSON.stringify(errors.length)
@@ -135,17 +139,17 @@ var parseUrl = function()
 {
 	var url = window.location.search
 	var queryParamsString = url.substring(1, url.length)
-	var queryParams = queryParamsString.split('&')
+	var queryParams = queryParamsString.split("&")
 	
 	if (queryParamsString.length)
 	{
 		queryParams.forEach(function(queryParam)
 		{
-			var splittedParam = queryParam.split('=')
+			var splittedParam = queryParam.split("=")
 			var param = splittedParam[0]
 			var value = splittedParam[1]
 
-			if (param === 'data')
+			if (param === "data")
 			{
 				try
 				{
@@ -155,6 +159,9 @@ var parseUrl = function()
 			}
 		})
 	}
+	
+	if (!("filenames" in data))
+		data.filenames = {}
 	mergeOptions()
 }
 
@@ -166,28 +173,36 @@ var mergeOptions = function()
 
 var refreshUI = function()
 {
+	if ("filenames" in data)
+	{
+		if ("output" in data.filenames)
+			outputFilename.value = data.filenames.output
+		
+		if ("schema" in data.filenames)
+			schemaFilename.value = data.filenames.schema
+	}
 	schemaTextarea.setValue(replaceSpacings(JSON.stringify(data.options.schema, null, 2)))
 	schemaTextarea.clearSelection(1)
 	validateSchema()
 	var themeMap = {
-		barebones: '',
-		bootstrap3: 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
-		bootstrap4: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-		bootstrap5: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css',
-		html: '',
-		spectre: 'https://unpkg.com/spectre.css/dist/spectre.min.css',
-		tailwind: 'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css'
+		barebones: "",
+		bootstrap3: "https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css",
+		bootstrap4: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+		bootstrap5: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+		html: "",
+		spectre: "https://unpkg.com/spectre.css/dist/spectre.min.css",
+		tailwind: "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
 	}
 	themeLink.href = themeMap[data.options.theme]
 	themeSelect.value = data.options.theme
 	var iconLibMap = {
-		fontawesome3: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.css',
-		fontawesome4: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css',
-		fontawesome5: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css',
-		jqueryui: 'https://code.jquery.com/ui/1.10.3/themes/south-street/jquery-ui.css',
-		openiconic: 'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic.min.css',
-		spectre: 'https://unpkg.com/spectre.css/dist/spectre-icons.min.css',
-		bootstrap: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css'
+		fontawesome3: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.css",
+		fontawesome4: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css",
+		fontawesome5: "https://use.fontawesome.com/releases/v5.6.1/css/all.css",
+		jqueryui: "https://code.jquery.com/ui/1.10.3/themes/south-street/jquery-ui.css",
+		openiconic: "https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic.min.css",
+		spectre: "https://unpkg.com/spectre.css/dist/spectre-icons.min.css",
+		bootstrap: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
 	}
 	iconlibLink.href = iconLibMap[data.options.iconlib]
 	iconlibSelect.value = data.options.iconlib
@@ -207,79 +222,79 @@ var refreshUI = function()
 	var libMapping = {
 		ace_editor: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/ace-editor-builds@1.2.4/src-min-noconflict/ace.js'
+				"https://cdn.jsdelivr.net/npm/ace-editor-builds@1.2.4/src-min-noconflict/ace.js"
 			],
 			css: []
 		},
 		choices: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js'
+				"https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css'
+				"https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css"
 			]
 		},
 		cleavejs: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/cleave.js@1.4.7/dist/cleave.min.js'
+				"https://cdn.jsdelivr.net/npm/cleave.js@1.4.7/dist/cleave.min.js"
 			],
 			css: []
 		},
 		sceditor: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
-				'https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/sceditor.min.js',
-				'https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/formats/bbcode.js',
-				'https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/formats/xhtml.js'
+				"https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js",
+				"https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/sceditor.min.js",
+				"https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/formats/bbcode.js",
+				"https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/formats/xhtml.js"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/themes/default.min.css'
+				"https://cdn.jsdelivr.net/npm/sceditor@2.1.3/minified/themes/default.min.css"
 			]
 		},
 		simplemde: {
 			js: [
-				'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js'
+				"https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css'
+				"https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css"
 			]
 		},
 		select2: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
-				'https://cdn.jsdelivr.net/npm/select2@4.0.6-rc.1/dist/js/select2.min.js'
+				"https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js",
+				"https://cdn.jsdelivr.net/npm/select2@4.0.6-rc.1/dist/js/select2.min.js"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/npm/select2@4.0.6-rc.1/dist/css/select2.min.css'
+				"https://cdn.jsdelivr.net/npm/select2@4.0.6-rc.1/dist/css/select2.min.css"
 			]
 		},
 		selectize: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
-				'https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/js/standalone/selectize.min.js'
+				"https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js",
+				"https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/js/standalone/selectize.min.js"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/css/selectize.min.css',
-				'https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/css/selectize.default.min.css'
+				"https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/css/selectize.min.css",
+				"https://cdn.jsdelivr.net/npm/selectize@0.12.6/dist/css/selectize.default.min.css"
 			]
 		},
 		flatpickr: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/flatpickr'
+				"https://cdn.jsdelivr.net/npm/flatpickr"
 			],
 			css: [
-				'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css'
+				"https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"
 			]
 		},
 		signature_pad: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js'
+				"https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"
 			],
 			css: []
 		},
 		mathjs: {
 			js: [
-				'https://cdn.jsdelivr.net/npm/mathjs@5.3.1/dist/math.min.js'
+				"https://cdn.jsdelivr.net/npm/mathjs@5.3.1/dist/math.min.js"
 			],
 			css: []
 		},
@@ -310,7 +325,7 @@ var refreshUI = function()
 			var concat = libMapping[selectedLib].js.concat(libMapping[selectedLib].css)
 			concat.forEach(function()
 			{
-				var className = '.external_' + selectedLib
+				var className = ".external_" + selectedLib
 				var toRemove = head.querySelector(className)
 				
 				if (toRemove)
@@ -323,20 +338,20 @@ var refreshUI = function()
 		{
 			libMapping[selectedLib].js.forEach(function(js)
 			{
-				var scriptElement = document.createElement('script')
-				scriptElement.type = 'text/javascript'
+				var scriptElement = document.createElement("script")
+				scriptElement.type = "text/javascript"
 				scriptElement.src = js
 				scriptElement.async = false
-				scriptElement.classList.add('external_' + selectedLib)
+				scriptElement.classList.add("external_" + selectedLib)
 				head.appendChild(scriptElement)
 			})
 			libMapping[selectedLib].css.forEach(function(css)
 			{
-				var linkElement = document.createElement('link')
-				linkElement.setAttribute('rel', 'stylesheet')
-				linkElement.setAttribute('type', 'text/css')
-				linkElement.setAttribute('href', css)
-				linkElement.classList.add('external_' + selectedLib)
+				var linkElement = document.createElement("link")
+				linkElement.setAttribute("rel", "stylesheet")
+				linkElement.setAttribute("type", "text/css")
+				linkElement.setAttribute("href", css)
+				linkElement.classList.add("external_" + selectedLib)
 				head.appendChild(linkElement)
 			})
 		})
@@ -351,7 +366,7 @@ var initJsoneditor = function()
 		jsoneditor.destroy()
 	}
 	jsoneditor = new window.JSONEditor(jsonEditorForm, data.options)
-	jsoneditor.on('change', function()
+	jsoneditor.on("change", function()
 	{
 		var json = jsoneditor.getValue()
 		outputTextarea.setValue(replaceSpacings(JSON.stringify(json, null, 2)))
@@ -364,38 +379,42 @@ var initJsoneditor = function()
 		}
 		else
 		{
-			validateTextarea.value = 'valid'
+			validateTextarea.value = "valid"
 		}
 	})
-	updateDirectLink()
 }
-
-var updateDirectLink = function()
+directLink.addEventListener("click", function()
 {
-	var url = window.location.href.replace(/\?.*/, '')
-	url += '?data='
+	var url = window.location.href.replace(/\?.*/, "")
+	url += "?data="
 	url += LZString.compressToBase64(JSON.stringify(data))
-	directLink.href = url
-}
-setOutput.addEventListener('click', function()
+	copyToClipboard(url)
+})
+resetButton.addEventListener("click", function()
+{
+	window.open("?", "_self")
+})
+setOutput.addEventListener("click", function()
 {
 	jsoneditor.setValue(JSON.parse(outputTextarea.getValue()))
 })
-copyOutput.addEventListener('click', function()
+copyOutput.addEventListener("click", function()
 {
 	copyToClipboard(outputTextarea.getValue())
-	document.documentElement.scrollTop = 0
-	document.body.scrollTop = 0
 })
-uploadOutput.addEventListener('click', function()
+openOutput.addEventListener("click", function()
 {
-	uploadFile(outputTextarea, setOutput)
+	openJSON(outputTextarea, setOutput)
 })
-downloadOutput.addEventListener('click', function()
+saveOutput.addEventListener("click", function()
 {
-	downloadFile(outputTextarea.getValue(), outputFilename.value + '.json', 'application/json')
+	openJSON(outputTextarea.getValue(), outputFilename.value + ".json", "application/json")
 })
-setSchema.addEventListener('click', function()
+outputFilename.addEventListener("change", function()
+{
+	data.filenames.output = outputFilename.value
+})
+setSchema.addEventListener("click", function()
 {
 	try
 	{
@@ -404,46 +423,48 @@ setSchema.addEventListener('click', function()
 	}
 	catch (e)
 	{
-		alert('Invalid Schema: ' + e.message)
+		alert("Invalid Schema: " + e.message)
 		return
 	}
 	refreshUI()
 })
-copySchema.addEventListener('click', function()
+copySchema.addEventListener("click", function()
 {
 	copyToClipboard(schemaTextarea.getValue())
-	document.documentElement.scrollTop = 0
-	document.body.scrollTop = 0
 })
-uploadSchema.addEventListener('click', function()
+openSchema.addEventListener("click", function()
 {
-	uploadFile(schemaTextarea, setSchema)
+	openJSON(schemaTextarea, setSchema)
 })
-downloadSchema.addEventListener('click', function()
+saveSchema.addEventListener("click", function()
 {
-	downloadFile(schemaTextarea.getValue(), schemaFilename.value + '.json', 'application/json')
+	saveJSON(schemaTextarea.getValue(), schemaFilename.value + ".json", "application/json")
 })
-themeSelect.addEventListener('change', function()
+schemaFilename.addEventListener("change", function()
 {
-	data.options.theme = this.value || ''
+	data.filenames.schema = schemaFilename.value
+})
+themeSelect.addEventListener("change", function()
+{
+	data.options.theme = this.value || ""
 	refreshUI()
 })
-iconlibSelect.addEventListener('change', function()
+iconlibSelect.addEventListener("change", function()
 {
-	data.options.iconlib = this.value || ''
+	data.options.iconlib = this.value || ""
 	refreshUI()
 })
-objectLayoutSelect.addEventListener('change', function()
+objectLayoutSelect.addEventListener("change", function()
 {
-	data.options.object_layout = this.value || ''
+	data.options.object_layout = this.value || ""
 	refreshUI()
 })
-showErrorsSelect.addEventListener('change', function()
+showErrorsSelect.addEventListener("change", function()
 {
-	data.options.show_errors = this.value || ''
+	data.options.show_errors = this.value || ""
 	refreshUI()
 })
-booleanOptionsSelect.addEventListener('change', function()
+booleanOptionsSelect.addEventListener("change", function()
 {
 	var booleanOptions = this.children
 	
@@ -453,7 +474,7 @@ booleanOptionsSelect.addEventListener('change', function()
 	}
 	refreshUI()
 })
-libSelect.addEventListener('change', function()
+libSelect.addEventListener("change", function()
 {
 	data.selectedLibs = []
 	data.unselectedLibs = []
